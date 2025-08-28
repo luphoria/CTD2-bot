be81c7741270b002eab5ff4a39f7525f95f93c77
LUPHORIA <luphoria@github.com>
2025-08-28 16:22:09 -0500

    Allow commands to run in any channel

--- index.js
+++ index.js
@@ -11,31 +11,29 @@ export const CHANNEL_ID = settings.CHANNEL_ID;
 
 // Bot functions
 client.on('messageCreate', async (message) => {
-    if (message.channel.id === CHANNEL_ID) {
-        const channel = client.channels.cache.get(CHANNEL_ID);
-        if (message.content.startsWith("!")) {
-            let res;
-            switch (message.content.split(" ")[0]) {
-                case "!help":
-                    await channel.send(`# Classic Tetris Daily Bot
+    const channel = client.channels.cache.get(message.channel.id);
+    if (message.content.startsWith("!")) {
+        let res;
+        switch (message.content.split(" ")[0]) {
+            case "!help":
+                await channel.send(`# Classic Tetris Daily Bot
 This bot generates a daily seed and posts it each day. 
  - !seed
     * Generates a seed. 
  - !setnext [seed]
     * Changes the next upcoming seed to a fixed one.
 `);
-                    break;
-                case "!seed":
-                    res = Seed(message.author.id);
-                    await channel.send(res.msg)
-                    break;
-                case "!setnext":
-                    res = SetNextSeed(message.author.id, message.content.split(" ")[1]);
-                    await channel.send(res.msg)
-                    break;
-                default:
-                    break;
-            }
+                break;
+            case "!seed":
+                res = Seed(message.author.id);
+                await channel.send(res.msg)
+                break;
+            case "!setnext":
+                res = SetNextSeed(message.author.id, message.content.split(" ")[1]);
+                await channel.send(res.msg)
+                break;
+            default:
+                break;
         }
     }
 })
