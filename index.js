dede62cc509e486da506a153343c56072ab1403a
LUPHORIA <luphoria@github.com>
2025-08-28 16:18:01 -0500

    remove comment

--- index.js
+++ index.js
@@ -41,9 +41,6 @@ This bot generates a daily seed and posts it each day.
 })
 
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
     initTimer();
